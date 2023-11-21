d2c7b04fc4dc7da1ff97ca54025bd5e78a6a5cd2
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-11-21 01:53:49 +0100

    added GenreInfo dialog functionalities

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -14,6 +14,7 @@ ngOnInit(): void {
   this.getMovies();
 }
 
+//gets all movies
 getMovies(): void {
   this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
@@ -22,3 +23,15 @@ getMovies(): void {
     });
   }
 }
+  // opens genres-info component
+  openGenreInfo(Genres: any[]): void {
+    console.log('Genre Object:', Genre);
+    this.dialog.open(GenreInfoComponent, {
+      data: {
+        Genre: Genre.Name,
+        Description: Genre.Description
+      }
+    });
+  }
+
+    // opens director-info component
